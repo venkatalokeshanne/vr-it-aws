08e7d9f2e7e24ede233c17521d4d0d824e526e5f
Venkata Lokesh Anne <annevlokesh@gmail.com>
2025-07-30 14:22:30 +0200

    Fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AnalyticsGoogle from "@/components/Analytics";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
